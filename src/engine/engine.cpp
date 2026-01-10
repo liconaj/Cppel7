e11bffa43e4dff5a852d5efce687f6333f3f2f90
Josué Licona <j.liconam@outlook.com>
2026-01-09 21:57:00 -0500

    feat: initialize virtual machine in engine

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -1,10 +1,22 @@
 #include "engine.h"
 
+#include "core/color_palette.h"
+#include "core/constants.h"
+#include "core/font_atlas.h"
+
 namespace cppel7 {
 
 Engine::Engine()
 {
     m_frameBuffer = std::make_unique<FrameBuffer>(m_config.width, m_config.height);
+
+    const int cells {m_config.width * m_config.height};
+    const int screenBufferSize {cells * SCREEN_BUFFER_BYTES_PER_CELL};
+    const int memorySize {ADDR_FIXED_MEMORY_END + screenBufferSize};
+    m_virtualMachine = std::make_unique<VirtualMachine>(memorySize);
+
+    uploadDefaultPalette();
+    uploadDefaultFontAtlas();
 }
 
 const Config& Engine::config()
@@ -30,6 +42,24 @@ void Engine::requestQuit()
 bool Engine::quitRequested() const
 {
     return m_quitRequested;
+}
+
+void Engine::uploadDefaultFontAtlas() const
+{
+    if (m_virtualMachine == nullptr) {
+        return;
+    }
+
+    m_virtualMachine->poke(ADDR_FONT_ATLAS_BASE, DEFAULT_FONT_ATLAS);
+}
+
+void Engine::uploadDefaultPalette() const
+{
+    if (m_virtualMachine == nullptr) {
+        return;
+    }
+
+    m_virtualMachine->poke(ADDR_PALETTE_BASE, DEFAULT_PALETTE);
 }
 
 } // namespace  cppel7
--- src/engine/engine.h
+++ src/engine/engine.h
@@ -5,6 +5,7 @@
 
 #include "core/config.h"
 #include "framebuffer.h"
+#include "virtual_machine/virtual_machine.h"
 
 namespace cppel7 {
 
@@ -24,8 +25,12 @@ public:
 private:
     Config m_config {};
     std::unique_ptr<FrameBuffer> m_frameBuffer;
+    std::unique_ptr<VirtualMachine> m_virtualMachine;
 
     bool m_quitRequested {false};
+
+    void uploadDefaultFontAtlas() const;
+    void uploadDefaultPalette() const;
 };
 
 } // namespace cppel7
